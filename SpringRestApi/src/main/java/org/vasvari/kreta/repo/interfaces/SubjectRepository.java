1395b57cc81d9b1765b7dadfc8b3d6fb197cab77
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-10-05 09:41:41 +0200

    Tymleaf projekt újraszervezve más mappába

--- SpringRestApi/src/main/java/org/vasvari/kreta/repo/interfaces/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package org.vasvari.kreta.repo.interfaces;
-
-
-import org.springframework.data.repository.CrudRepository;
-import org.vasvari.kreta.model.Subject;
-public interface SubjectRepository extends CrudRepository<Subject,Long> {
-}
